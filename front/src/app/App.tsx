3a51b7e52891fd58c360aa7d167d1488f2bd8f1e
Igor Filchev <filchev.igor@gmail.com>
2022-05-27 00:27:18 +0300

    5-add-routing-to-the-app-file

--- front/src/app/App.tsx
+++ front/src/app/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 
-const App = () : JSX.Element => {
-  const data = 55555;
-
-  return (
-    <div>{data}</div>
-  );
-};
+const App = () : JSX.Element => (
+  <div className="App">
+    <h1>Welcome to React Router!</h1>
+    <Routes>
+      { /* <Route path="/" element={<Home />} /> */}
+    </Routes>
+  </div>
+);
 
 export default App;
--- front/src/index.tsx
+++ front/src/index.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 // eslint-disable-next-line import/no-unresolved
 import App from './app/App';
-import './index.css';
 // eslint-disable-next-line import/no-unresolved
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 );
 
